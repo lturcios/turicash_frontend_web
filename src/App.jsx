09c6def4cf7b8dbfe9a19dad093bb6a0f4c6aa45
Luis Turcios <l.turcios.08@gmail.com>
2026-05-07 22:48:56 -0600

    refactor: comment out unused route paths in MainLayout

--- src/App.jsx
+++ src/App.jsx
@@ -167,9 +167,9 @@ const MainLayout = ({ onLogout }) => {
           <Routes>
             <Route path="/" element={<DashboardPage />} />
             <Route path="/items" element={<ItemsPage />} />
-            <Route path="/categories" element={<CategoriesPage />} />
+            {/* <Route path="/categories" element={<CategoriesPage />} /> */}
             <Route path="/users" element={<UsersPage />} />
-            <Route path="/locations" element={<LocationsPage />} />
+            {/* <Route path="/locations" element={<LocationsPage />} /> */}
             <Route path="/history" element={<HistoryPage />} />
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
